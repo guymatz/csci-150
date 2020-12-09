17d173afe284287399acdbe80743adc3fc34197e
Guy Matz <guy.matz@schrodinger.com>
2020-12-09 15:14:12 -0500

    added recursive algorithm found on internet

--- eea.py
+++ eea.py
@@ -4,12 +4,28 @@ import math
 import sys
 
 
+def gcdExtended(a, b):  
+    print("gcdExtended")
+    # Base Case  
+    if a == 0 :   
+        return b,0,1
+             
+    gcd,x1,y1 = gcdExtended(b%a, a)  
+     
+    # Update x and y using results of recursive  
+    # call  
+    x = y1 - (b//a) * x1  
+    y = x1  
+     
+    return gcd,x,y
+
 def egcd(a, b):
     r = [a, b]
     x = [1, 0]
     y = [0, 1]
     i = 2
     while (r[i-2] % r[i-1]) > 0:
+        print("egcd")
         print(r)
         rt = r[i-2] % r[i-1]
         r.append(rt)
@@ -24,3 +40,4 @@ def egcd(a, b):
     return(x,y)
 
 print(egcd(int(sys.argv[1]), int(sys.argv[2])))
+print(gcdExtended(int(sys.argv[1]), int(sys.argv[2])))
